3eaa8fecb3f99abf326175bd6d15cc701a567f49
Jirapus <jirapus@blockfint.com>
2019-09-04 11:26:07 +0700

    Remove unused code

--- abci/did/v1/checkTx.go
+++ abci/did/v1/checkTx.go
@@ -48,29 +48,8 @@ var IsMethod = map[string]bool{
 	"SetValidator":       true,
 }
 
-type BigInt struct {
-	big.Int
-}
-
 type SignValues struct {
 	R, S *big.Int
-}
-
-func (b BigInt) MarshalJSON() ([]byte, error) {
-	return []byte(b.String()), nil
-}
-
-func (b *BigInt) UnmarshalJSON(p []byte) error {
-	if string(p) == "null" {
-		return nil
-	}
-	var z big.Int
-	_, ok := z.SetString(string(p), 10)
-	if !ok {
-		return fmt.Errorf("not a valid big integer: %s", p)
-	}
-	b.Int = z
-	return nil
 }
 
 func (app *DIDApplication) checkCanRegisterMasterNode(param string, nodeID string) types.ResponseCheckTx {
